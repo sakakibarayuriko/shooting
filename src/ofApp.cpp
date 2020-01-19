b0c0b72614d516a5aaa9dffe8fdeafe18698ff43
榊原ゆり子 <sakakibarayuriko@sakakibarayuriko-no-MacBook-Air.local>
2020-01-19 18:31:37 +0900

    リファクタリング

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,479 +2,229 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    //ofBackground(0);
     ofSetFrameRate(60);
     ofSetCircleResolution(64);
     ofEnableBlendMode(OF_BLENDMODE_ADD);
     
-    //loadfont
+    //フォント読み込み
     ofTrueTypeFont::setGlobalDpi(72);
     font1.load("SFNSText.ttf", 30);
     font2.load("SFNSText.ttf", 60);
     
-    //Light
+    //ライト設定
     ofEnableLighting();
     light.setPosition(512, 0, 50);
     light.enable();
     
-    Enemy = 8;
+    //プレーヤーの数
     Player = 1;
+    //敵の数
+    Enemy = 8;
     
-    //Enemy
-    //drawRect = true;
-    loc_x1 = 256;
-    loc_y1 = 384;
-    loc_z1 = -1000;
+    //敵の弾
+    loc_x[0] = 256;
+    loc_y[0] = 384;
+    loc_z[0] = -1000;
     
-    loc_x2 = 512;
-    loc_y2 = 200;
-    loc_z2 = -1000;
+    loc_x[1] = 512;
+    loc_y[1] = 200;
+    loc_z[1] = -1000;
     
-    loc_x3 = 768;
-    loc_y3 = 384;
-    loc_z3 = -1000;
+    loc_x[2] = 768;
+    loc_y[2] = 384;
+    loc_z[2] = -1000;
     
-    loc_x4 = 512;
-    loc_y4 = 600;
-    loc_z4 = -1000;
+    loc_x[3] = 512;
+    loc_y[3] = 600;
+    loc_z[3] = -1000;
     
-    loc_x5 = 100;
-    loc_y5 = 100;
-    loc_z5 = -1000;
+    loc_x[4] = 100;
+    loc_y[4] = 100;
+    loc_z[4] = -1000;
     
-    loc_x6 = 924;
-    loc_y6 = 100;
-    loc_z6 = -1000;
+    loc_x[5] = 924;
+    loc_y[5] = 100;
+    loc_z[5] = -1000;
     
-    loc_x7 = 924;
-    loc_y7 = 668;
-    loc_z7 = -1000;
+    loc_x[6] = 924;
+    loc_y[6] = 668;
+    loc_z[6] = -1000;
     
-    loc_x8 = 100;
-    loc_y8 = 668;
-    loc_z8 = -1000;
+    loc_x[7] = 100;
+    loc_y[7] = 668;
+    loc_z[7] = -1000;
     
-    drawInformation = true;
     drawPlayer = true;
-    drawEnemy = true;
-    drawEnemy2 = true;
-    drawEnemy3 = true;
-    drawEnemy4 = true;
-    drawEnemy5 = true;
-    drawEnemy6 = true;
-    drawEnemy7 = true;
-    drawEnemy8 = true;
-    countEnemy = true;
-    countEnemy2 = true;
-    countEnemy3 = true;
-    countEnemy4 = true;
-    countEnemy5 = true;
-    countEnemy6 = true;
-    countEnemy7 = true;
-    countEnemy8 = true;
+    for(int i = 0; i < 8; i++) {
+        drawEnemy[i] = true;
+        countEnemy[i] = true;
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    //弾の加速度
+    z -= 10;
     
-    loc_x1 = loc_x1 + 0;
-    loc_y1 = loc_y1 + 0;
-    loc_z1 = loc_z1 + 8;
-    
-    loc_x2 = loc_x2 + 0;
-    loc_y2 = loc_y2 + 0;
-    loc_z2 = loc_z2 + 8;
-    
-    loc_x3 = loc_x3 + 0;
-    loc_y3 = loc_y3 + 0;
-    loc_z3 = loc_z3 + 8;
-    
-    loc_x4 = loc_x4 + 0;
-    loc_y4 = loc_y4 + 0;
-    loc_z4 = loc_z4 + 8;
-    
-    loc_x5 = loc_x5 + 0;
-    loc_y5 = loc_y5 + 0;
-    loc_z5 = loc_z5 + 8;
-    
-    loc_x6 = loc_x6 + 0;
-    loc_y6 = loc_y6 + 0;
-    loc_z6 = loc_z6 + 8;
-    
-    loc_x7 = loc_x7 + 0;
-    loc_y7 = loc_y7 + 0;
-    loc_z7 = loc_z7 + 8;
-    
-    loc_x8 = loc_x8 + 0;
-    loc_y8 = loc_y8 + 0;
-    loc_z8 = loc_z8 + 8;
-    
-    if(loc_z1 == 0){
-        loc_x1 = 256;
-        loc_y1 = 384;
-        loc_z1 = -1000;
+    for(int i = 0; i < 8; i++) {
+        loc_x[i] = loc_x[i] + 0;
+        loc_y[i] = loc_y[i] + 0;
+        loc_z[i] = loc_z[i] + 8;
     }
     
-    if(loc_z2 == 0){
-        loc_x2 = 512;
-        loc_y2 = 200;
-        loc_z2 = -1000;
+    //弾を撃ち続ける
+    if (z == -1000) {
+        z = 0;
     }
     
-    if(loc_z3 == 0){
-        loc_x3 = 768;
-        loc_y3 = 384;
-        loc_z3 = -1000;
+    if(loc_z[0] == 0){
+        loc_x[0] = 256;
+        loc_y[0] = 384;
+        loc_z[0] = -1000;
     }
     
-    if(loc_z4 == 0){
-        loc_x4 = 512;
-        loc_y4 = 600;
-        loc_z4 = -1000;
+    if(loc_z[1] == 0){
+        loc_x[1] = 512;
+        loc_y[1] = 200;
+        loc_z[1] = -1000;
     }
     
-    if(loc_z5 == 0){
-        loc_x5 = 100;
-        loc_y5 = 100;
-        loc_z5 = -1000;
+    if(loc_z[2] == 0){
+        loc_x[2] = 768;
+        loc_y[2] = 384;
+        loc_z[2] = -1000;
     }
     
-    if(loc_z6 == 0){
-        loc_x6 = 924;
-        loc_y6 = 100;
-        loc_z6 = -1000;
+    if(loc_z[3] == 0){
+        loc_x[3] = 512;
+        loc_y[3] = 600;
+        loc_z[3] = -1000;
     }
     
-    if(loc_z7 == 0){
-        loc_x7 = 924;
-        loc_y7 = 668;
-        loc_z7 = -1000;
+    if(loc_z[4] == 0){
+        loc_x[4] = 100;
+        loc_y[4] = 100;
+        loc_z[4] = -1000;
     }
     
-    if(loc_z8 == 0){
-        loc_x8 = 100;
-        loc_y8 = 668;
-        loc_z8 = -1000;
+    if(loc_z[5] == 0){
+        loc_x[5] = 924;
+        loc_y[5] = 100;
+        loc_z[5] = -1000;
+    }
+    
+    if(loc_z[6] == 0){
+        loc_x[6] = 924;
+        loc_y[6] = 668;
+        loc_z[6] = -1000;
+    }
+    
+    if(loc_z[7] == 0){
+        loc_x[7] = 100;
+        loc_y[7] = 668;
+        loc_z[7] = -1000;
     }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    ofBackground(0);
-    
-    if(drawInformation){
+    //ゲーム中（プレーヤーかつ敵が残っている）
+    if(Enemy != 0 && Player != 0) {
+        //
+        ofBackground(0);
         ofSetColor(255);
         font1.drawString(ofToString(Enemy) + "enemys", 450, 35);
-    }
-    
-    //Player
-    ofPushMatrix();
-    if(drawPlayer){
-        ofSetColor(31, 63, 255);
-        ofDrawCircle(mouseX, mouseY,20);
-        ofSetColor(116, 193, 206);
-        sphere.set(6,10);
-        sphere.setPosition(mouseX, mouseY, z);
-        sphere.draw();
-        z -= velocity;
-        if (z == -1000) {
-            z = 0;
+        
+        //プレーヤー
+        ofPushMatrix();
+        if(drawPlayer){
+            //プレーヤー自身描画
+            ofSetColor(31, 63, 255);
+            ofDrawCircle(mouseX, mouseY,20);
+            ofSetColor(116, 193, 206);
+            //弾描画
+            sphere.set(6,10);
+            sphere.setPosition(mouseX, mouseY, z);
+            sphere.draw();
+        }
+        ofPopMatrix();
+        
+        //敵
+        ofPushMatrix();
+        for(int i = 0; i < 8; i++) {
+            if(drawEnemy[i]){
+                //敵自身描画
+                ofSetColor(255);
+                box.set(40);
+                box.setPosition(loc_x[i], loc_y[i], -1000);
+                box.draw();
+                //弾描画
+                ofSetColor(211, 24, 24);
+                sphere.set(6,10);
+                sphere.setPosition(loc_x[i], loc_y[i], loc_z[i]);
+                sphere.draw();
+            }
+        }
+        ofPopMatrix();
+        
+        //当たり判定
+        for(int i = 0; i < 8; i++) {
+            playerHitCheck(i);
+            enemyHitCheck(i);
         }
     }
-    
-    ofPopMatrix();
-    
-    //Enemy
-    ofPushMatrix();
-    if(drawEnemy){
-        ofSetColor(255);
-        box.set(40);
-        box.setPosition(loc_x1, loc_y1, -1000);
-        box.draw();
-        ofSetColor(211, 24, 24);
-        sphere.set(6,10);
-        sphere.setPosition(loc_x1, loc_y1, loc_z1);
-        sphere.draw();
-    }
-    ofPopMatrix();
-    
-    ofPushMatrix();
-    if(drawEnemy2){
-        ofSetColor(255);
-        box.set(40);
-        box.setPosition(loc_x2, loc_y2, -1000);
-        box.draw();
-        ofSetColor(211, 24, 24);
-        sphere.set(6,10);
-        sphere.setPosition(loc_x2, loc_y2, loc_z2);
-        sphere.draw();
-    }
-    ofPopMatrix();
-    
-    ofPushMatrix();
-    if(drawEnemy3){
-        ofSetColor(255);
-        box.set(40);
-        box.setPosition(loc_x3, loc_y3, -1000);
-        box.draw();
-        ofSetColor(211, 24, 24);
-        sphere.set(6,10);
-        sphere.setPosition(loc_x3, loc_y3, loc_z3);
-        sphere.draw();
-    }
-    ofPopMatrix();
-    
-    ofPushMatrix();
-    if(drawEnemy4){
-        ofSetColor(255);
-        box.set(40);
-        box.setPosition(loc_x4, loc_y4, -1000);
-        box.draw();
-        ofSetColor(211, 24, 24);
-        sphere.set(6,10);
-        sphere.setPosition(loc_x4, loc_y4, loc_z4);
-        sphere.draw();
-    }
-    ofPopMatrix();
-    
-    ofPushMatrix();
-    if(drawEnemy5){
-        ofSetColor(255);
-        box.set(40);
-        box.setPosition(loc_x5, loc_y5, -1000);
-        box.draw();
-        ofSetColor(211, 24, 24);
-        sphere.set(6,10);
-        sphere.setPosition(loc_x5, loc_y5, loc_z5);
-        sphere.draw();
-    }
-    ofPopMatrix();
-    
-    ofPushMatrix();
-    if(drawEnemy6){
-        ofSetColor(255);
-        box.set(40);
-        box.setPosition(loc_x6, loc_y6, -1000);
-        box.draw();
-        ofSetColor(211, 24, 24);
-        sphere.set(6,10);
-        sphere.setPosition(loc_x6, loc_y6, loc_z6);
-        sphere.draw();
-    }
-    ofPopMatrix();
-    
-    ofPushMatrix();
-    if(drawEnemy7){
-        ofSetColor(255);
-        box.set(40);
-        box.setPosition(loc_x7, loc_y7, -1000);
-        box.draw();
-        ofSetColor(211, 24, 24);
-        sphere.set(6,10);
-        sphere.setPosition(loc_x7, loc_y7, loc_z7);
-        sphere.draw();
-    }
-    ofPopMatrix();
-    
-    ofPushMatrix();
-    if(drawEnemy8){
-        ofSetColor(255);
-        box.set(40);
-        box.setPosition(loc_x8, loc_y8, -1000);
-        box.draw();
-        ofSetColor(211, 24, 24);
-        sphere.set(6,10);
-        sphere.setPosition(loc_x8, loc_y8, loc_z8);
-        sphere.draw();
-    }
-    ofPopMatrix();
-    
-    if(Player != 0){
-        HitCheck();
-        HitCheck2();
-        HitCheck3();
-        HitCheck4();
-        HitCheck5();
-        HitCheck6();
-        HitCheck7();
-        HitCheck8();
-        HitCheck9();
-        gameClear();
-    }
-    gameOver();
+    //ゲーム終了
+    isFinished();
 }
 
 //--------------------------------------------------------------
-void ofApp::HitCheck(){
+void ofApp::playerHitCheck(int i){
     //敵がプレーヤーに当てる
-    if(!(ofDist(mouseX, mouseY, 0, loc_x1, loc_y1, loc_z1)<=23 || ofDist(mouseX, mouseY, 0, loc_x2, loc_y2, loc_z2)<=23
-         || ofDist(mouseX, mouseY, 0, loc_x3, loc_y3, loc_z3)<=23 || ofDist(mouseX, mouseY, 0, loc_x4, loc_y4, loc_z4)<=23
-         || ofDist(mouseX, mouseY, 0, loc_x5, loc_y5, loc_z5)<=23 || ofDist(mouseX, mouseY, 0, loc_x6, loc_y6, loc_z6)<=23
-         || ofDist(mouseX, mouseY, 0, loc_x7, loc_y7, loc_z7)<=23 || ofDist(mouseX, mouseY, 0, loc_x8, loc_y8, loc_z8)<=23)){
+    if(!(ofDist(mouseX, mouseY, 0, loc_x[i], loc_y[i], loc_z[i])<=23 && countEnemy[i])){
         return;
     }
+    //プレーヤーが減る
     Player -= 1;
 }
 
 //--------------------------------------------------------------
-void ofApp::HitCheck2(){
+void ofApp::enemyHitCheck(int i){
     //プレーヤーが敵に当てる
-    if(!(ofDist(mouseX, mouseY, z, loc_x1, loc_y1, -1000)<=23)){
+    if(!(ofDist(mouseX, mouseY, z, loc_x[i], loc_y[i], -1000)<=23)){
         return;
     }
-    drawEnemy = false;
-    loc_z1 = -1000;
-    if(countEnemy){
+    drawEnemy[i] = false;
+    loc_z[i] = -1000;
+    if(countEnemy[i]){
+        //敵が減る
         Enemy -= 1;
         ofSetColor(211, 24, 24);
-        ofDrawCircle(loc_x1, loc_y1, loc_z1, 30);
-        countEnemy = false;
+        ofDrawCircle(loc_x[i], loc_y[i], loc_z[i], 30);
+        countEnemy[i] = false;
     }
 }
 
 //--------------------------------------------------------------
-void ofApp::HitCheck3(){
-    //プレーヤーが敵に当てる
-    if(!(ofDist(mouseX, mouseY, z, loc_x2, loc_y2, -1000)<=23)){
-        return;
-    }
-    drawEnemy2 = false;
-    loc_z2 = -1000;
-    if(countEnemy2){
-        Enemy -= 1;
-        ofSetColor(211, 24, 24);
-        ofDrawCircle(loc_x2, loc_y2, loc_z2, 30);
-        countEnemy2 = false;
-    }
-}
-
-//--------------------------------------------------------------
-void ofApp::HitCheck4(){
-    //プレーヤーが敵に当てる
-    if(!(ofDist(mouseX, mouseY, z, loc_x3, loc_y3, -1000)<=23)){
-        return;
-    }
-    drawEnemy3 = false;
-    loc_z3 = -1000;
-    if(countEnemy3){
-        Enemy -= 1;
-        ofSetColor(211, 24, 24);
-        ofDrawCircle(loc_x3, loc_y3, loc_z3, 30);
-        countEnemy3 = false;
-    }
-}
-
-//--------------------------------------------------------------
-void ofApp::HitCheck5(){
-    //プレーヤーが敵に当てる
-    if(!(ofDist(mouseX, mouseY, z, loc_x4, loc_y4, -1000)<=23)){
-        return;
-    }
-    drawEnemy4 = false;
-    loc_z4 = -1000;
-    if(countEnemy4){
-        Enemy -= 1;
-        ofSetColor(211, 24, 24);
-        ofDrawCircle(loc_x4, loc_y4, loc_z4, 30);
-        countEnemy4 = false;
-    }
-}
-
-//--------------------------------------------------------------
-void ofApp::HitCheck6(){
-    //プレーヤーが敵に当てる
-    if(!(ofDist(mouseX, mouseY, z, loc_x5, loc_y5, -1000)<=23)){
-        return;
-    }
-    drawEnemy5 = false;
-    loc_z5 = -1000;
-    if(countEnemy5){
-        Enemy -= 1;
-        ofSetColor(211, 24, 24);
-        ofDrawCircle(loc_x5, loc_y5, loc_z5, 30);
-        countEnemy5 = false;
-    }
-}
-
-//--------------------------------------------------------------
-void ofApp::HitCheck7(){
-    //プレーヤーが敵に当てる
-    if(!(ofDist(mouseX, mouseY, z, loc_x6, loc_y6, -1000)<=23)){
-        return;
-    }
-    drawEnemy6 = false;
-    loc_z6 = -1000;
-    if(countEnemy6){
-        Enemy -= 1;
-        ofSetColor(211, 24, 24);
-        ofDrawCircle(loc_x6, loc_y6, loc_z6, 30);
-        countEnemy6 = false;
-    }
-}
-
-//--------------------------------------------------------------
-void ofApp::HitCheck8(){
-    //プレーヤーが敵に当てる
-    if(!(ofDist(mouseX, mouseY, z, loc_x7, loc_y7, -1000)<=23)){
-        return;
-    }
-    drawEnemy7 = false;
-    loc_z7 = -1000;
-    if(countEnemy7){
-        Enemy -= 1;
-        ofSetColor(211, 24, 24);
-        ofDrawCircle(loc_x7, loc_y7, loc_z7, 30);
-        countEnemy7 = false;
-    }
-}
-
-//--------------------------------------------------------------
-void ofApp::HitCheck9(){
-    //プレーヤーが敵に当てる
-    if(!(ofDist(mouseX, mouseY, z, loc_x8, loc_y8, -1000)<=23)){
-        return;
-    }
-    drawEnemy8 = false;
-    loc_z8 = -1000;
-    if(countEnemy8){
-        Enemy -= 1;
-        ofSetColor(211, 24, 24);
-        ofDrawCircle(loc_x8, loc_y8, loc_z8, 30);
-        countEnemy8 = false;
-    }
-}
-
-//--------------------------------------------------------------
-void ofApp::gameClear(){
+void ofApp::isFinished(){
+    //敵が消滅
     if(Enemy == 0){
         drawPlayer = false;
-        drawInformation = false;
-        //gameclear
+        //ゲームクリア
         ofSetColor(256, 256, 0);
         font2.drawString("GameClear!", 350, 384);
     }
-    return;
-}
-
-//--------------------------------------------------------------
-void ofApp::gameOver(){
+    //プレーヤーが消滅
     if(Player == 0){
-        drawEnemy = false;
-        drawEnemy2 = false;
-        drawEnemy3 = false;
-        drawEnemy4 = false;
-        drawEnemy5 = false;
-        drawEnemy6 = false;
-        drawEnemy7 = false;
-        drawEnemy8 = false;
-        drawPlayer = false;
-        drawInformation = false;
-        //gameover
+        for(int i = 0; i < 8; i++) {
+            drawEnemy[i] = false;
+        }
+        //ゲームオーバー
         ofSetColor(256, 256, 0);
         font2.drawString("GameOver!", 350, 384);
     }
     return;
 }
+
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
     
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,22 +2,16 @@
 
 #include "ofMain.h"
 
+#define LENGTH 8
+
 class ofApp : public ofBaseApp{
 public:
     void setup();
     void update();
     void draw();
-    void HitCheck();
-    void HitCheck2();
-    void HitCheck3();
-    void HitCheck4();
-    void HitCheck5();
-    void HitCheck6();
-    void HitCheck7();
-    void HitCheck8();
-    void HitCheck9();
-    void gameClear();
-    void gameOver();
+    void playerHitCheck(int i);
+    void enemyHitCheck(int i);
+    void isFinished();
     
     void keyPressed(int key);
     void keyReleased(int key);
@@ -36,19 +30,11 @@ public:
     ofLight light;
     ofTrueTypeFont font1, font2;
     
-    int Enemy, Player;
-    bool drawEnemy, drawEnemy2, drawEnemy3, drawEnemy4, drawEnemy5, drawEnemy6, drawEnemy7, drawEnemy8, drawPlayer;
-    bool countEnemy, countEnemy2, countEnemy3, countEnemy4, countEnemy5, countEnemy6, countEnemy7, countEnemy8;
-    bool drawInformation;
-    float loc_x1, loc_y1, loc_z1;
-    float loc_x2, loc_y2, loc_z2;
-    float loc_x3, loc_y3, loc_z3;
-    float loc_x4, loc_y4, loc_z4;
-    float loc_x5, loc_y5, loc_z5;
-    float loc_x6, loc_y6, loc_z6;
-    float loc_x7, loc_y7, loc_z7;
-    float loc_x8, loc_y8, loc_z8;
+    int Player;
+    int z;
     
-    int velocity = 10;  //オブジェクトのスピード
-    int z = 0;  //オブジェクトのx座標
+    int Enemy;
+    bool drawEnemy[LENGTH], drawPlayer;
+    bool countEnemy[LENGTH];
+    float loc_x[LENGTH], loc_y[LENGTH], loc_z[LENGTH];
 };
